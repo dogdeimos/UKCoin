74470974e8a887e1a1f30e00674b6acf04bf834e
UKCDev <31538795+UKCDev@users.noreply.github.com>
2017-09-16 23:05:04 +0100

    Stake Threshold And Time Weight Adjusted

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -26,13 +26,13 @@ int64_t nReserveBalance = 0;
 int64_t nMinimumInputValue = 0;
 
 static unsigned int GetStakeSplitAge() { return 8 * 24 * 60 * 60; }
-static int64_t GetStakeCombineThreshold() { return 50 * COIN; }
+static int64_t GetStakeCombineThreshold() { return 50000 * COIN; }
 
 int64_t gcd(int64_t n,int64_t m) { return m == 0 ? n : gcd(m, n % m); }
 static uint64_t CoinWeightCost(const COutput &out)
 {
     int64_t nTimeWeight = (int64_t)GetTime() - (int64_t)out.tx->nTime;
-    CBigNum bnCoinDayWeight = CBigNum(out.tx->vout[out.i].nValue) * nTimeWeight / (24 * 60 * 60);
+    CBigNum bnCoinDayWeight = CBigNum(out.tx->vout[out.i].nValue) * nTimeWeight / (1 * 24 * 60 * 60);
     return bnCoinDayWeight.getuint64();
 }
 
